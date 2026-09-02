87703f0263d7b74bb0bef9f072bb1868b8227d29
Lokanadham <152874001+Loka746@users.noreply.github.com>
2026-09-02 14:40:00 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = 0
+        if sorted(s) == sorted(t):
+
+            return True
+
+        else: 
+            return False
+
+
+
+       
+
+
+
+               
+
+                
+        
